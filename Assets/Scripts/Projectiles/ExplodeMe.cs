27bf347c8a7fd52a47b4a4ce405888577aed568e
Ryan Kilkenny <ilmaestro@gmail.com>
2015-02-08 20:12:14 -0800

    added projectile
    tank can now fire a projectile

---
+++ Assets/Scripts/Projectiles/ExplodeMe.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodeMe : MonoBehaviour {
+    
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.gameObject.name);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Units/UnitController.cs
+++ Assets/Scripts/Units/UnitController.cs
@@ -10,9 +10,13 @@ public class UnitController : MonoBehaviour {
     public float maxSpeed = 20f;
     public float acceleration = 2f;
     public float damping = 0.9f;
+    public GameObject projectile;
+    public Transform firePoint;
+    public float fireForce = 50f;
+    public float fireRate = 1f;
 
     private float currentSpeed = 0f;
-
+    private float lastFireTime = 0f;
     void Awake() 
     { 
 
@@ -21,22 +25,37 @@ public class UnitController : MonoBehaviour {
 	void Start () 
     {
         currentSpeed = 0f;
+        lastFireTime = Time.time;
 	}
 	
 	void Update () 
     {
         CheckPosition();
         CheckRotation();
+        CheckFire();
 	}
 
-    void CheckRotation()
+    private void CheckFire()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= lastFireTime)
+        {
+            GameObject proj = (GameObject)Instantiate(projectile, firePoint.position, firePoint.rotation);
+            proj.GetComponent<ShootMe>().Fire(fireForce);
+            //proj.rigidbody.AddForce(firePoint.position.normalized * fireForce);
+
+            lastFireTime = Time.time + fireRate;
+        }
+
+    }
+
+    private void CheckRotation()
     {
         Vector3 targetRotation = new Vector3(0, Input.GetAxis("Horizontal") * acceleration, 0) + transform.rotation.eulerAngles;
         transform.rotation =  Quaternion.Euler(targetRotation);
         
     }
 
-    void CheckPosition()
+    private void CheckPosition()
     {
         //check for speed adjustments
         if (currentSpeed >= -maxSpeed && currentSpeed <= maxSpeed)
